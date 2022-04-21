da2dbf5a3df82e703401a1593f935e2d58caa90c
anderson <anderson.ap133@gmail.com>
2022-04-21 14:42:31 -0300

    Updated Enemy Movement

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ class Grid {
 
 const player = new Player()
 const projectiles = []
-const grids = [new Grid()]
+const grids = []
 const keys = {
     a: {
         pressed: false
@@ -171,6 +171,11 @@ const keys = {
         pressed: false
     }
 }
+
+let frames = 0
+let randomInterval = Math.floor(Math.random() * 500 + 500)
+
+console.log(randomInterval)
 
 function animate() {
     requestAnimationFrame(animate)
@@ -206,6 +211,17 @@ function animate() {
         player.velocity.x = 0
         player.rotation = 0
     }
+
+    console.log(frames)
+
+    if (frames % randomInterval === 0) {
+        grids.push(new Grid())
+        randomInterval = Math.floor(Math.random() * 500 + 500)
+        frames = 0
+        console.log(randomInterval)
+    }
+
+    frames++
 }
 
 animate()
