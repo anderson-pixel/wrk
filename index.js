1c3ac8fa9264a7c85c5657dca6e1b44c7125f5a5
anderson <anderson.ap133@gmail.com>
2022-04-22 15:54:21 -0300

    Added Moving Background

--- index.js
+++ index.js
@@ -73,13 +73,14 @@ class Projectile {
 }
 
 class Particle {
-     constructor({position, velocity, radius, color }) {
+     constructor({position, velocity, radius, color, fades }) {
         this.position = position
         this.velocity = velocity
 
         this.radius = radius
         this.color = color
         this.opacity = 1
+        this.fades = fades
     }
 
     draw() {
@@ -98,6 +99,7 @@ class Particle {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
+        if (this.fades)
         this.opacity -= 0.01
     }
 }
@@ -242,7 +244,23 @@ const keys = {
 let frames = 0
 let randomInterval = Math.floor(Math.random() * 500 + 500)
 
-function createParticles({object, color}) {
+ for (let i = 0; i < 100; i++) {
+        particles.push(new Particle({
+           position: {
+           x: Math.random() * canvas.width,
+           y: Math.random() * canvas.height
+           },
+           velocity: {
+           x: 0,
+           y: 0.3
+           }, 
+           radius: Math.random() * 2,
+           color: 'white'
+        })
+       )
+    }
+
+function createParticles({object, color, fades}) {
     for (let i = 0; i < 15; i++) {
         particles.push(new Particle({
            position: {
@@ -254,7 +272,8 @@ function createParticles({object, color}) {
            y: (Math.random() - 0.5) * 2
            }, 
            radius: Math.random() * 3,
-           color: color || '#BAA0DE'
+           color: color || '#BAA0DE', 
+           fades
         })
        )
     }
@@ -266,6 +285,12 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     particles.forEach((particle, i) => {
+
+        if (particle.position.y - particle.radius >= canvas.height) {
+            particle.position.x = Math.random() * canvas.width
+            particle.position.y = -particle.radius
+        }
+
         if (particle.opacity <= 0) {
             setTimeout(() => {
                  particles.splice(i, 1)
@@ -289,7 +314,8 @@ function animate() {
             console.log('u lose')
              createParticles({
             object: player,
-            color: 'white'
+            color: 'white',
+            fades: true
             })
         }
     })
@@ -335,7 +361,8 @@ function animate() {
 
                         if (invaderFound && projectileFound) {
                         createParticles({
-                            object: invader
+                            object: invader,
+                            fades: true
                         })
 
                         grid.invaders.splice(i, 1)
